91416b82acc735c159e70ed844077c5dfbcca63c
Sam McCormick <samjmccormick@gmail.com>
2024-09-09 16:14:35 -0700

    completed HTML mostly finished CSS, will need to style the results divs once they're being created in JS started JS by assigning elements to variables

---
+++ script.js
@@ -0,0 +1,5 @@
+const numberInput = document.getElementById("number");
+const checkBtn = document.getElementById("check-btn");
+const clearBtn = document.getElementById("clear-btn");
+const resultsDiv = document.getElementById("results-div");
+
