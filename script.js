41f476b5f6415dde3bfb069a3fa424802eb9b17b
Sam McCormick <samjmccormick@gmail.com>
2024-09-09 20:00:40 -0700

    got the regular expressions done, should cover all the cases it seems to be correctly identifying phone numbers with true or false now just need to add HTML elements for the results and figure out how to clear all of those HTML elements when its done

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
-const numberInput = document.getElementById("number");
+const userInput = document.getElementById("user-input");
 const checkBtn = document.getElementById("check-btn");
 const clearBtn = document.getElementById("clear-btn");
 const resultsDiv = document.getElementById("results-div");
 
+/* thoughts:
+0. if the input is empty put out alert 
+1. create a regex that will confirm if a number is in the correct format
+	a. is this really the whole thing? both seems too easy and too hard lol 
+	b. do I need to creat a different regex for each formatting case? 
+	c. is it worth cleaning up the input first just to make sure there are the right amount of numbers? 
+2. use the regex to put out the alert if it isn't formatted correctly 
+3. clean up the string so it's just numbers
+4. if it's 11 digits long then the first digit needs to be 1 to match the US global code
+5. if it is formatted correctly, add a child to the resultsDIV with the text "Valid US number: " + input.value 
+6. if it isn't formatted correctly, add a child to the resultsDiv with the text "Invalid US number: " + input.value 
+7. clear button needs to remove all the child Div elements from the restults div 
+
+  */
+
+const regexAreaCode1 = /^\([0-9]{3}\)\s{0,1}[0-9]{3}(-|\s{0,1})[0-9]{4}$/; /*checks variations of area code in parenthesis */
+const regexAreaCode2 = /^[0-9]{3}(-|\s{0,1})[0-9]{3}(-|\s{0,1})[0-9]{4}$/; /*checks area code no parenthesis */
+const regexCountryCode1 = /^1\s{0,1}\([0-9]{3}\)\s{0,1}[0-9]{3}(-|\s{0,1})[0-9]{4}$/; /*checks for country code only matches with 1, and parenthasis */
+const regexCountryCode2 =  /^1\s{0,1}[0-9]{3}(-|\s{0,1})[0-9]{3}(-|\s{0,1})[0-9]{4}$/; /* country code but no parenthesis */
+
+const validPhoneNumbers = [regexAreaCode1, regexAreaCode2, regexCountryCode1, regexCountryCode2];
+
+function isValid (num) {
+	return validPhoneNumbers.some((regex) => regex.test(num));
+}
+
+/* need to either figure out append child, add innerHTML maybe? */
+resultsDiv.appendChild(document.createElement("p").setAttribute("class", "results").innerHTML);
+
+function checkUserInput (input) {
+	if (input === "") {
+		alert("Please provide a phone number") 
+	} else if (isValid(input)) {
+		resultsDiv.appendChild(``)
+	}
+}
+
+
+
+checkBtn.addEventListener("click", () => {checkUserInput(userInput.value)});
